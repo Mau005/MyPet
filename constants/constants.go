23f4d6783f725e6d60bc2575252759eadb588977
Mauri <mpino1701@gmail.com>
2024-05-02 18:22:08 -0400

    add error and privileges

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,21 @@
 package constants
 
 const (
+	ERROR_ACCESS_CREDENTIALS = "access credentials error"
+	ERROR_TOKEN              = "token validation error"
+	ERROR_GET_TOKEN          = "error searching for token"
+	ERROR_LEN_PASSWORD       = "password length too short"
+	ERROR_UNHAUTORIZED       = "you do not have privileges to perform this action"
+)
+
+const (
+	PRIVILEGES_USER = iota
+	PRIVILEGES_MODERATOR
+	PRIVILEGES_ADMINISTRATOR
+	PRIVILEGES_SUPER_ADMIN
+)
+
+const (
 	LEN_PASSWORD     = 8
 	LEN_ACCOUNT_NAME = 3
 )
@@ -10,5 +25,5 @@ const (
 )
 
 const (
-	ACCOUNT_SESSION = "account_session"
+	ACCOUNT_SESSION = "Authorization"
 )
